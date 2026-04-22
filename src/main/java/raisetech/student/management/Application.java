9eabae3401c6158be57a59e83683934d7bb4fbb6
YT1Quynh <tranthiyquynh1992@gmail.com>
2026-04-22 16:44:08 +0900

    addLombok

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -1,16 +1,10 @@
 package raisetech.student.management;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -21,33 +15,14 @@ public class Application {
 	private StudentRepository repository;
 
 
-	private String name = "EnamiKouji";
-	private int age = 21;
-
 	public static void main(String[] args) {
+
 		SpringApplication.run(Application.class, args);
 	}
 
-	@GetMapping("/student")
-	public String getStudent(@RequestParam String name){
-		Student student = repository.searchByName(name);
-		return student.getName()+ " " + student.getAge() + "歳";
-
-	}
-
-	@PostMapping("/student")
-	public void registerStudent(String name, int age){
-		repository.registerStudent(name, age);
-
-	}
-	@PatchMapping("/student")
-	public void updateStudentName(String name, int age){
-		repository.updateStudent(name, age);
-	}
-
-	@DeleteMapping("/student")
-	public void deleteStudent(String name){
-		repository.deleteStudent(name);
+	@GetMapping("/studentList")
+	public List<Student> getStudentList(){
+		return repository.search();
 
 	}
 }
